d40226c89e4e6fd857d99d4f6af8fc70325f1fa5
bui quang huy <Codatduoc@gmail.com>
2020-10-19 17:24:51 +0700

    demo home screen

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,11 @@ import React, { lazy, Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import LoadingIndicator from './components/LoadingIndicator';
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel/dist/assets/owl.theme.default.css';
 import './stylesheets/index.scss';
+import 'react-dates/initialize';
+import 'react-dates/lib/css/_datepicker.css';
 import configureStore from './store/store';
 import * as serviceWorker from './serviceWorker';
 
